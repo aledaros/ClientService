36f5d5a9254c79317b969d1cc8638132b1579092
Mattia <mattia.ceschin@stud.tecnicosuperiorekennedy.it>
2021-12-02 17:20:20 +0100

    Add Order repository

---
+++ src/Gruppo3.Clienti/Gruppo3.Clienti.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gruppo4MicroserviziDTO.DTOs;
+
+namespace Gruppo3.Clienti.Application.Interfaces.Repositories
+{
+    public interface IOrderRepository
+    {
+        //insert
+        public NewOrderEvent InsertOrder(NewOrderEvent orderEvent);
+
+        //update
+        public UpdatedOrderEvent UpdateOrder(UpdatedOrderEvent orderEvent);
+
+        //delete
+        public DeletedOrderEvent DeleteOrder(DeletedOrderEvent orderEvent);
+    }
+}
---
+++ src/Gruppo3.Clienti/Gruppo3.Clienti.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using Gruppo3.Clienti.Application.Interfaces.Repositories;
+using Gruppo4MicroserviziDTO.DTOs;
+using Microsoft.Extensions.Configuration;
+using RepoDb;
+using System.Data.SqlClient;
+
+namespace Gruppo3.Clienti.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly IConfiguration _configuration;
+        private const string NAME_DB = "DB";
+
+        public OrderRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DeletedOrderEvent DeleteOrder(DeletedOrderEvent orderEvent)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString(NAME_DB)))
+            {
+                connection.Delete(orderEvent);
+                return orderEvent;
+            }
+        }
+
+        public NewOrderEvent InsertOrder(NewOrderEvent orderEvent)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString(NAME_DB)))
+            {
+                connection.Insert(orderEvent);
+                return orderEvent;
+            }
+        }
+
+        public UpdatedOrderEvent UpdateOrder(UpdatedOrderEvent orderEvent)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString(NAME_DB)))
+            {
+                connection.Update(orderEvent);
+                return orderEvent;
+            }
+        }
+    }
+}

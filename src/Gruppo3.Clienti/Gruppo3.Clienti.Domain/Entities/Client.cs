ced966223697c3a14089e877353f59e5bb1f125d
Ale <darosalessandro1@gmail.com>
2021-12-02 16:05:19 +0100

    Update Client.cs

--- src/Gruppo3.Clienti/Gruppo3.Clienti.Domain/Entities/Client.cs
+++ src/Gruppo3.Clienti/Gruppo3.Clienti.Domain/Entities/Client.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gruppo3.Clienti.Domain.Entities
+﻿namespace Gruppo3.Clienti.Domain.Entities
 {
     public class Client
     {
